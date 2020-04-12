8d7b5cc9db1f8fab42b8b0da1e10df704fe54f1f
PeonyF <1621139@gmail.com>
2020-04-13 03:34:59 +0900

    first commit

---
+++ app/src/main/java/com/example/viewpagerdemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.viewpagerdemo;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupActionBar();
+        getViews();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setTitle("Tab Layout");
+        }
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+
+    private void getViews() {
+        toolbar = findViewById(R.id.toolBar);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+    }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new FragmentOne(), "One");
+        viewPagerAdapter.addFragment(new FragmentTwo(), "Two");
+        viewPagerAdapter.addFragment(new FragmentThree(), "Three");
+        viewPagerAdapter.addFragment(new FragmentFour(), "Four");
+        viewPagerAdapter.addFragment(new FragmentFive(), "Five");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
